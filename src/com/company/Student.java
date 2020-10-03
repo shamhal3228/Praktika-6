dcf708a2566051991eb139abd5bda8e6ba3e1df4
shamhal3228 <56553437+shamhal3228@users.noreply.github.com>
2020-10-03 22:52:07 +0300

    Add files via upload

---
+++ src/com/company/Student.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Student {
+    private int id;
+    Student(int id)
+    {
+        this.id=id;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+}
